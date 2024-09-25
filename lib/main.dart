41518984ce813b05bc4705248739989f469f4359
sreerag-13 <sreeragsaji41@gmail.com>
2024-09-25 20:13:02 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(Patient());
+}
+class Patient extends StatelessWidget {
+  const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
